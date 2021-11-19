20a6d9ff6480e100ef5449b6079780a97b241551
DDROZD <damien.drozd@ynov.com>
2021-11-19 22:44:04 +0100

    test

--- src/game/Game.java
+++ src/game/Game.java
@@ -71,33 +71,7 @@ public class Game {
 		}
 	}
 	
-	public static void isDame(Pion[] tabJoueur1, Pion[] tabJoueur2) {
-		
-		for (int i = 0; i<tabJoueur2.length ; i++) {
-			
-			if (tabJoueur2[i].getPos() >= 1 && tabJoueur2[i].getPos() <= 5) {
-				tabJoueur2[i].setType("dame") ;
-				// tabJoueur2[i] =  new King(i+1,null, 0,  "O", 1);
-			} 
-			
-		}
-		for (int i = 0; i<tabJoueur1.length ; i++) {
-			
-			if (tabJoueur1[i].getPos() >= 46 && tabJoueur1[i].getPos() <= 50) {
-				tabJoueur1[i].setType("dame") ;
-			} 
-			
-		}
-	}
 	
-	public static boolean playerLoose(Pion[] tabJoueur) {
-		for (int i = 0; i<tabJoueur.length ; i++) {
-			if (tabJoueur[i].isDead() == false) {
-				return false;
-			} 
-		}
-		return true;
-	}
 }
 
 
